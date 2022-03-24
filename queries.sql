99453ba5ce9552474b1b69477fbd195cf16f3b2c
Gia <gmay925@yahoo.com>
2022-03-23 22:23:48 -0400

    final

--- queries.sql
+++ queries.sql
@@ -1,6 +1,10 @@
--- Part 1: Test it with SQL
-SELECT * FROM job;
-
+-- Part 1: Test it with SQ;
+CREATE TABLE job (
+id int,
+employer varchar(100),
+name varchar(100),
+skill varchar(120)
+);
 -- Part 2: Test it with SQL
 SELECT name
 FROM employer
@@ -11,6 +15,6 @@ DROP TABLE job;
 
 -- Part 4: Test it with SQL
 SELECT * FROM skill
-INNER JOIN job_skills ON  job_skills.skills_id = skill.id
+LEFT JOIN job_skills ON  job_skills.skills_id = skill.id
 WHERE job_skills.jobs_id IS NOT NULL
 ORDER BY name ASC;
